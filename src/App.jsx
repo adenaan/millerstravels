0da83c770d976fc125f313e4cd2a2de4793e4c63
Adenaan <adenaan@gmail.com>
2026-02-04 22:16:21 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
-
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import Packages from './pages/Packages'
-import Contact from './pages/Contact'
-import AdminLogin from './pages/AdminLogin'
-import AdminDashboard from './pages/AdminDashboard'
-import AdminRoute from './components/AdminRoute'
-import { Routes, Route, useLocation } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { ensureSeed } from './utils/storage'
+import { defaultPackages, defaultConfig } from './data/defaultData'
+import Layout from './components/Layout'
+import Home from './components/Home'
+import PackagesPage from './components/PackagesPage'
+import PackageDetails from './components/PackageDetails'
+import AdminLogin from './components/admin/AdminLogin'
+import AdminDashboard from './components/admin/AdminDashboard'
+import ProtectedRoute from './components/admin/ProtectedRoute'
 
 export default function App(){
-  const location = useLocation()
+  useEffect(() => {
+    ensureSeed({ defaultPackages, defaultConfig })
+  }, [])
+
   return (
-    <div>
-      <Header />
-      <Routes location={location}>
-        <Route path="/" element={<Home />} />
-        <Route path="/packages" element={<Packages />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/admin/login" element={<AdminLogin />} />
-        <Route path="/admin" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
-      </Routes>
-      <Footer />
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/packages" element={<PackagesPage />} />
+        <Route path="/packages/:id" element={<PackageDetails />} />
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route
+          path="/admin/dashboard"
+          element={
+            <ProtectedRoute>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
   )
 }
