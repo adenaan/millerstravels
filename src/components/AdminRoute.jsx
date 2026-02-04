a182159cee9dafcdf7c66b207a583bba0a175528
Adenaan <adenaan@gmail.com>
2026-02-04 22:15:49 +0200

    Delete src/components directory

--- src/components/AdminRoute.jsx
+++
@@ -1,8 +0,0 @@
-
-import { Navigate } from 'react-router-dom'
-import { isAuthenticated } from '../store/auth'
-
-export default function AdminRoute({children}){
-  if(!isAuthenticated()) return <Navigate to="/admin/login" replace />
-  return children
-}
